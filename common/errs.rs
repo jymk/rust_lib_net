332d7a8c0a2add5e4d33fec3a292c30e1074e8dd
jymk <sakamototsune@163.com>
2022-08-24 01:25:54 +0000

    update common/errs.rs. 更改暴露范围

--- common/errs.rs
+++ common/errs.rs
@@ -1,17 +1,17 @@
 use std::fmt::{Debug, Display};
 
 /// 请求格式错误
-pub const ERR_FORMAT: &'static str = "格式错误";
+pub(crate) const ERR_FORMAT: &'static str = "格式错误";
 /// 服务器错误
-pub const ERR_SERVER_INTERNAL: &'static str = "服务器内部错误";
+pub(crate) const ERR_SERVER_INTERNAL: &'static str = "服务器内部错误";
 
 /// 格式错误
-pub fn err_format<T>() -> SResult<T> {
+pub(crate) fn err_format<T>() -> SResult<T> {
     sresult_from_err(ERR_FORMAT)
 }
 
 /// 服务器内部错误
-pub fn err_server_internal<T>() -> SResult<T> {
+pub(crate) fn err_server_internal<T>() -> SResult<T> {
     sresult_from_err(ERR_SERVER_INTERNAL)
 }
 
